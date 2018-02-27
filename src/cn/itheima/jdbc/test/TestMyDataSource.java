f10197c4ac479641632361fd1afba0381e535c4f
wang920321 <m13372782171_1@163.com>
2018-02-27 16:45:04 +0800

    自定义连接池改进（增强close方法）

--- src/cn/itheima/jdbc/test/TestMyDataSource.java
+++ src/cn/itheima/jdbc/test/TestMyDataSource.java
@@ -4,12 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
 
 import cn.itheima.jdbc.DataSource.MyDataSource;
+import cn.itheima.jdbc.DataSource.MyDataSource1;
+import cn.itheima.jdbc.utils.JDBCUtil_V3;
 
 public class TestMyDataSource {
 	
+    /**
+     * 添加用户
+     * 使用未改造过的Connection
+     */
     @Test
     public void testAddUser(){
     	Connection conn=null;
@@ -35,6 +43,38 @@ public class TestMyDataSource {
 			e.printStackTrace();
 		}finally{
 			dataSource.backConnection(conn);
+		}
+    }
+    /**
+     * 添加用户
+     * 使用造过的Connection
+     */
+    @Test
+    public void testAddUser1(){
+    	Connection conn=null;
+    	PreparedStatement pst=null;
+    	//1创建自定义连接池对象
+    	DataSource dataSource=new MyDataSource1();
+    	try {
+    		//2从连接池获取连接
+			conn=dataSource.getConnection();
+			String sql="insert into user values(null,?,?)";
+			//3必须在自定义的MyConnection类中重写preparStatement(sql)方法
+			pst=conn.prepareStatement(sql);
+			pst.setString(1, "张凡1");
+			pst.setString(2, "12121");
+			int row = pst.executeUpdate();
+			if(row>0){
+				System.out.println("添加成功");
+			}else{
+				System.out.println("失败");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			JDBCUtil_V3.release(conn, pst, null);
 		}
     }
 }
